e87bdf0cad6b34f6d02e6b6049ba9c60f2559c2e
AMMAR KHODJA Hichem <hichem5696@gmail.com>
2020-02-23 17:24:53 +0100

    Update text_preprocessor.py

--- printed_text_dataset/text_preprocessor.py
+++ printed_text_dataset/text_preprocessor.py
@@ -1,4 +1,5 @@
 from printed_text_dataset.sequence.sequence import Text, Sentence, Word, Punctuation, Separator
+from printed_text_dataset.sequence_r.sequence_rendering import WordR, TextR, ParagraphR
 from printed_text_dataset.text_generation.distribution import Distribution
 
 from PIL import ImageFont
@@ -28,21 +29,25 @@ class TextPreprocessor:
             margin[k] = self.margin_distrib[k].sample()
 
         text_r.margin = margin
-
+        last_word = None
+        temp_sentences = []
+        paragraphs = []
         for sent_sep in text:
             if isinstance(sent_sep, Sentence):
+                temp_sentences.append(sent_sep)
                 for word_sep_pun in sent_sep:
                     if isinstance(word_sep_pun, (Word, Punctuation)):
                         word_r = WordR(components=word_sep_pun)
-                        word_r.spacing = self.word_spacing_distrib.sample()
-                        if self.font_level = Level.WORD:
-                            word_r.setFont(self.sample_font())
-                            word_r.generate_bb()
+                        last_word = word_r
+                        word_r.spacing = 0
+                    elif isinstance(word_sep_pun, Separator):
+                        last_word.spacing += self.word_spacing_distrib.sample()
+            elif isinstance(sent_sep, Separator):
+                paragraphs.append(ParagraphR(temp_sentences))
+                temp_sentences = []
+
 
     def sample_font(self):
         font_name, size = self.font_distrib.sample(), self.font_size_distrib.sample()
         font = ImageFont.truetype(font_name, size)
         return font
-
-
-
